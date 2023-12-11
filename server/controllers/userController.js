14ceb773302f7440b5358cdf33500aaef161c9cb
Yahor Yankavets <mr.yankovec@gmail.com>
2023-12-11 23:18:00 +0300

    added new functions

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,17 @@
-class userController {
+const ApiError = require("../error/ApiError");
+
+class UserController {
   async registration(req, res) {}
 
   async login(req, res) {}
 
-  async check(req, res) {}
+  async check(req, res, next) {
+    const { id } = req.query;
+    if (!id) {
+      return next(ApiError.badRequest("Не указан ID"));
+    }
+    res.json(id);
+  }
 }
 
-module.exports = new userController();
+module.exports = new UserController();
